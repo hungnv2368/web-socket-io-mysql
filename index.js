8a91341957f2a29f71334ec1f2959ef3e57bf872
vanhung.nguyen <vanhung.nguyen@Nguyens-MacBook-Pro.local>
2020-06-03 21:49:58 +0700

    them index

--- index.js
+++ index.js
@@ -26,12 +26,10 @@ var maxIdUserRequest = 0;
 io.sockets.on('connection', function (socket) {
     socketCount++
     io.sockets.emit('users connected', socketCount)
-
     socket.on('disconnect', function () {
         socketCount--
         io.sockets.emit('users connected', socketCount)
     })
-
     socket.on('new note', function (data) {
         // New note added, push to all sockets and insert into db
         notes.push(data)
@@ -48,30 +46,96 @@ io.sockets.on('connection', function (socket) {
             })
             .on('end', function () {
                 // Only emit notes after query has been completed
-                socket.emit('initial devices', devices);
+                io.sockets.emit('initial devices', devices);
             })
         isInitDevices = true
     } else {
         // Initial notes already exist, send out
-        socket.emit('initial devices', devices)
+        io.sockets.emit('initial devices', devices)
     }
-    db.query('select max(id) as id from user_requests')
-        .on('result', function (data) {
-            // Push results onto the notes array
-            if (!data.id)
-                maxIdUserRequest = 0;
-            else maxIdUserRequest = data.id;
-        })
-        .on('end', function (dt) {
-            // Only emit notes after query has been completed
-            socket.emit('max user_requests', maxIdUserRequest);
-        })
+
     socket.on('max user_requests', function (data) {
-        io.sockets.emit('max user_requests', maxIdUserRequest);
+        console.log('get max user request');
+        db.query('select max(id) as id from user_requests')
+            .on('result', function (data) {
+                // Push results onto the notes array
+                if (!data.id)
+                    maxIdUserRequest = 0;
+                else maxIdUserRequest = data.id;
+                console.log('lay duoc max user request: ' + maxIdUserRequest);
+            })
+            .on('end', function (dt) {
+                // Only emit notes after query has been completed
+                io.sockets.emit('max user_requests', maxIdUserRequest);
+                console.log('send noti max user request ' + maxIdUserRequest);
+            })
     })
     socket.on('new user_requests', function (data) {
-        db.query(`insert into user_requests (id, bandwidth) values(?, ?)`, data.lastRequestID, data.req_bandwidth);
-        io.sockets.emit('new user_requests', maxIdUserRequest++);
+        try {
+            console.log('insert user request ' + data.lastRequestID);
+            db.query(`insert into user_requests (id, bandwidth) values(${data.lastRequestID},${data.req_bandwidth})`)
+                .on('result', function (dx) {
+                    console.log('insert xong user request');
+                })
+                .on('end', function (dt) {
+                    // Only emit notes after query has been completed
+                    io.sockets.emit('new user_requests', data.lastRequestID);
+                    console.log('send noti new user request ' + data.lastRequestID);
+                })
+
+        } catch (error) {
+            console.error(error);
+        }
+
+    })
+    socket.on('new regions', function (data) {
+        try {
+            console.log('insert regions' + data.id);
+            db.query(`insert into regions (id, lati_north, longti_east, lati_south, longti_west)
+             values(${data.id},${data.lati_north},${data.longti_east},${data.lati_south},${data.longti_west})`)
+                .on('result', function (dx) {
+                    console.log('insert xong regions');
+                })
+                .on('end', function (dt) {
+                    //console.log('send noti new region ' + data.id);
+                })
+
+        } catch (error) {
+            console.error(error);
+        }
+    })
+    socket.on('new regions_of_request', function (data) {
+        try {
+            console.log('insert regions_of_request ' + data.usr_request_id);
+            db.query(`insert into regions_of_request (usr_request_id, region_id)
+             values(${data.usr_request_id},${data.region_id})`, function name(a, b, c) {
+                var dd = 2;
+            })
+                .on('result', function (dx) {
+                    console.log('insert xong regions_of_request');
+                })
+                .on('end', function (dt) {
+                    //console.log('send noti new region ' + data.id);
+                })
+
+        } catch (error) {
+            console.error(error);
+        }
+
+    })
+    socket.on('update regions', function (data) {
+        try {
+            console.log('update regions' + data.device_id);
+            db.query(`update regions set device_id=${data.device_id} where id=${data.region_id}`)
+                .on('result', function (dx) {
+                    console.log('update xong regions');
+                })
+                .on('end', function (dt) {
+                })
+
+        } catch (error) {
+            console.error(error);
+        }
 
     })
 
